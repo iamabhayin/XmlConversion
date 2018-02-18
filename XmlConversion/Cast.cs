11310f53a68fe6dad7b0a4f78a5a842cb3492803
iamabhayin <iamabhayin@gmail.com>
2018-02-18 23:32:42 +0530

    Added Xml to DataTable conversion methods..

--- XmlConversion/Cast.cs
+++ XmlConversion/Cast.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace XmlConversion
 {
     public class Cast
     {
+        public Cast()
+        {
+
+        }
+
+        public static DataTable ToDataTable(string xml)
+        {
+            var dataTable = ConvertToDataTable.ToDataTable(xml);
+            return dataTable;
+        }
     }
 }
---
+++ XmlConversion/ConvertToDataTable.cs
@@ -0,0 +1,46 @@
+﻿namespace XmlConversion
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+    internal class ConvertToDataTable
+    {
+        readonly static string dataTableName = "Table1";
+        public static DataTable ToDataTable(string _xml)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(new StringReader(_xml));
+            DataTable dataTable = new DataTable(doc.FirstChild.Name);
+            try
+            {
+
+                XmlNode node = doc.FirstChild.FirstChild;
+                XmlNode childNode = doc.FirstChild;
+                foreach (XmlNode columna in node.ChildNodes)
+                {
+                    dataTable.Columns.Add(columna.Name, typeof(String));
+                }
+                foreach (XmlNode cNode in childNode.ChildNodes)
+                {
+                    List<string> values = new List<string>();
+                    foreach (XmlNode Columna in cNode.ChildNodes)
+                    { 
+                        values.Add(Columna.InnerText);
+                    }
+                    dataTable.Rows.Add(values.ToArray());
+                }
+
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
